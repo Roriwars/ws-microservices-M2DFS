39e06cf82dbdeea8623d5159751b6cf298f3e4a9
Robin <robin2b2010@gmail.com>
2020-10-24 00:07:26 +0200

    Partie 6 - Serveur Eureka

--- src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
+++ src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
@@ -2,10 +2,9 @@ package com.ecommerce.microcommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
-@EnableSwagger2
+@EnableEurekaClient
 public class MicrocommerceApplication {
 
 	public static void main(String[] args) {
