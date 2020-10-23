9d32017368fab31266be26cdf7b05d81f7797776
Robin <robin2b2010@gmail.com>
2020-10-23 13:38:15 +0200

    new project

--- src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.microcommerce.web.exceptions;
-
-public class ProduitGratuitException extends RuntimeException {
-    public ProduitGratuitException(String e){
-        super(e);
-    }
-}
